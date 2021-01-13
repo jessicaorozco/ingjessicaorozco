6234e9d6674ce745d6a858833eef9d56e7b06a51
Jessica Orozco <jessicaorozco@gmail.com>
2021-01-13 14:12:26 -0500

    primer commit

---
+++ java/scripts.js
@@ -0,0 +1,18 @@
+
+  // DOM is loaded and ready for manipulation here
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("nav-icon").style.display= "none";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("nav-icon").style.display= "block";
+}
+
